03bd1704027c2927531cd2719f2f3f6fe406af6b
rasu204 <59288730+rasu204@users.noreply.github.com>
2020-09-09 03:32:38 -0700

    Update urls.py

--- recruitment/pipeline/urls.py
+++ recruitment/pipeline/urls.py
@@ -3,7 +3,9 @@ from . import views
 from django.contrib import admin
 
 urlpatterns = [
-    path('',views.pipeline, name='pipeline'),
-    path('excel_import/',views.excel_import, name='excel_import')
-
+    #path('',views.pipeline, name='pipeline'),
+    #path('excel_import',views.excel_import, name='excel_import'),
+    #path('fieldmatching',views.fieldmatching, name='fieldmatching'),
+    path('import', views.import_data),
+    path('fieldmatching', views.fieldmatching)
 ]
